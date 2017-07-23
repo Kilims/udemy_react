3d9327fa6a0fe279b7ac6f398f6f1b64b9761417
kilims <516759403@qq.com>
2017-07-23 23:46:44 +0800

    class 25 done and green --- add modal window to app

--- public/bundle.js
+++ public/bundle.js
@@ -44792,10 +44792,25 @@ var Cart = function (_React$Component) {
     function Cart() {
         _classCallCheck(this, Cart);
 
-        return _possibleConstructorReturn(this, (Cart.__proto__ || Object.getPrototypeOf(Cart)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (Cart.__proto__ || Object.getPrototypeOf(Cart)).call(this));
+
+        _this.state = {
+            showModal: false
+        };
+        return _this;
     }
 
     _createClass(Cart, [{
+        key: 'open',
+        value: function open() {
+            this.setState({ showModal: true });
+        }
+    }, {
+        key: 'close',
+        value: function close() {
+            this.setState({ showModal: false });
+        }
+    }, {
         key: 'onDelete',
         value: function onDelete(_id) {
             var currentBookToDelete = this.props.cart;
@@ -44910,7 +44925,70 @@ var Cart = function (_React$Component) {
             return _react2.default.createElement(
                 _reactBootstrap.Panel,
                 { header: 'Cart', bsStyle: 'primary' },
-                cartItemsList
+                cartItemsList,
+                _react2.default.createElement(
+                    _reactBootstrap.Row,
+                    null,
+                    _react2.default.createElement(
+                        _reactBootstrap.Col,
+                        { xs: 12 },
+                        _react2.default.createElement(
+                            'h6',
+                            null,
+                            'Total amoutn:'
+                        ),
+                        _react2.default.createElement(
+                            _reactBootstrap.Button,
+                            { onClick: this.open.bind(this), bsStyle: 'success' },
+                            'PROCEED TO CHECKOUT'
+                        )
+                    )
+                ),
+                _react2.default.createElement(
+                    _reactBootstrap.Modal,
+                    { show: this.state.showModal, onHide: this.close.bind(this) },
+                    _react2.default.createElement(
+                        _reactBootstrap.Modal.Header,
+                        { closeButton: true },
+                        _react2.default.createElement(
+                            _reactBootstrap.Modal.Title,
+                            null,
+                            'Thank your !'
+                        )
+                    ),
+                    _react2.default.createElement(
+                        _reactBootstrap.Modal.Body,
+                        null,
+                        _react2.default.createElement(
+                            'h6',
+                            null,
+                            'Your order has been saved'
+                        ),
+                        _react2.default.createElement(
+                            'p',
+                            null,
+                            'You will receive an email confirmation'
+                        )
+                    ),
+                    _react2.default.createElement(
+                        _reactBootstrap.Modal.Footer,
+                        null,
+                        _react2.default.createElement(
+                            _reactBootstrap.Col,
+                            { xs: 6 },
+                            _react2.default.createElement(
+                                'h6',
+                                null,
+                                'T                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   otal $.'
+                            )
+                        ),
+                        _react2.default.createElement(
+                            _reactBootstrap.Button,
+                            { onClick: this.close.bind(this) },
+                            'Close'
+                        )
+                    )
+                )
             );
         }
     }]);
