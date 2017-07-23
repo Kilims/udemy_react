d2db78cd0830bf0a8c31d1dfe4006c1878c1b44a
kilims <516759403@qq.com>
2017-07-23 16:08:25 +0800

    udemy class 19 done and green -- create booksForm component

---
+++ src/components/booksForm.js
@@ -0,0 +1,38 @@
+"use strict"
+import React from 'react';
+import {Well, Panel, FormControl, FormGroup, ControlLabel, Button} from 'react-bootstrap';
+
+class BooksForm extends React.Component{
+    render(){
+        return(
+            <Well>
+                <Panel>
+                    <FormGroup controlId="title" >
+                        <ControlLabel>Title</ControlLabel>
+                        <FormControl 
+                            type="text"
+                            placeholder="Enter title"
+                            ref="title" />
+                    </FormGroup>
+                    <FormGroup controlId="description" >
+                        <ControlLabel>Description</ControlLabel>
+                        <FormControl 
+                            type="text"
+                            placeholder="Enter description"
+                            ref="description" />
+                    </FormGroup>
+                    <FormGroup controlId="price" >
+                        <ControlLabel>Price</ControlLabel>
+                        <FormControl 
+                            type="text"
+                            placeholder="Enter price"
+                            ref="price" />
+                    </FormGroup>
+                    <Button bsStyle="primary">Buy now</Button>
+                </Panel>
+            </Well>
+        )
+    }
+}
+
+export default BooksForm;
--- src/components/booksList.js
+++ src/components/booksList.js
@@ -6,6 +6,7 @@ import { getBooks } from '../actions/booksActions';
 import {Grid, Row, Col, Button} from 'react-bootstrap';
 
 import BookItem from './bookItem';
+import BooksForm from './booksForm';
 
 class BookList extends React.Component{
     componentDidMount() {
@@ -27,7 +28,10 @@ class BookList extends React.Component{
         })
         return (
             <Grid>
-                <Row style={{marginTop:'15px'}}>
+                <Row>
+                    <Col xs={12} sm={6}>
+                        <BooksForm />
+                    </Col>
                     {booksList}
                 </Row>
             </Grid>
